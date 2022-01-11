e846e745c7209d60189ad02061bc9f97cae17dd5
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-01-11 17:33:50 +0900

    check mobile bug

--- theme/js/main.js
+++ theme/js/main.js
@@ -2,10 +2,10 @@
 const filter = "win16|win32|win64|mac|macintel";
 if (navigator.platform) {
     if (filter.indexOf(navigator.platform.toLowerCase()) < 0) { //mobile 
-        // alert('mobile 접속!');
+        alert('mobile 접속!');
         const status_bar = $('#status-bar');
         status_bar.css("visibility", "hidden");
     } else { //pc 
-        // alert('pc 접속!');
+        alert('pc 접속!');
     }
 }
